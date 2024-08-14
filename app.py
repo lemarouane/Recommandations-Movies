06e600a8b85a1023a3e86e2bd93a60e028278c1f
MAROUANE HADDAD <143835897+lemarouane@users.noreply.github.com>
2024-08-14 12:51:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,18 @@ def fetch_poster(movie_id):
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
+    return recommended_movie_names,recommended_movie_posters
 
 
 st.header('Un système de Recommandation des films avec Machine learning et Intelligence artificielle.')
