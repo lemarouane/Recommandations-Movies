8763dfa094a4d66a159b8c234cbb7cf899d52725
MAROUANE HADDAD <143835897+lemarouane@users.noreply.github.com>
2024-08-14 12:50:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import pickle
-import streamlit as st
+
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
